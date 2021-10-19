70b7db3f9576564b977e9c26fc6852abe5d2d20a
juandsoto <juandsoto1006@gmail.com>
2021-10-19 12:39:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+const typeDefs = require("./graphql/typeDefs.js");
+const resolvers = require("./graphql/resolvers/index.js");
+const { MONGODB } = require("./config.js");
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => console.log(`Server running at ${res.url}`));
